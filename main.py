6f1004298e1f5f51fbcc70317ef16a0e5ab01f46
Zagir <azazoha@gmail.com>
2026-02-16 21:20:45 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("api key not found, check .env")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+    if response.usage_metadata == None:
+        raise RuntimeError("usage metadata is None, API request failed")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
